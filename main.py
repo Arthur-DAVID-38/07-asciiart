134d76484d24ddaa735785c51ec040f8d9ef4671
Arthur-DAVID-38 <arthur.david@edu.esiee.fr>
2025-09-29 12:24:54 +0000

    algo iteratif & recursif pour exo tuples

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-#### Imports et définition des variables globales
+
+"""Module d'encodage ASCII art avec algorithmes itératif et récursif."""
 
 # Mandatory for the recursive solution to work on large inputs
 import sys
@@ -9,43 +10,48 @@ sys.setrecursionlimit(2000)
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
-
+    """
+    Retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif.
     Args:
         s (str): la chaîne de caractères à encoder
-
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    return [ ]
-
+    l = []
+    count = 1
+    current_car = s[0]
+    for car in s[1:]:
+        if car == current_car:
+            count += 1
+        else:
+            l.append((current_car, count))
+            current_car = car
+            count = 1
+    l.append((current_car, count))
+    return l
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
-
+    """
+    Retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif.
     Args:
         s (str): la chaîne de caractères à encoder
-
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
-
-    return []
-    
+    if len(s) == 0:
+        return []
+    if len(s) == 1:
+        return [(s[0], 1)]
+    rest = artcode_r(s[1:])
+    if s[0] == rest[0][0]:
+        return [(s[0], rest[0][1] + 1)] + rest[1:]
+    return [(s[0], 1)] + rest
 
 #### Fonction principale
 
 
 def main():
+    """Fonction principale pour tester les algorithmes d'encodage."""
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
